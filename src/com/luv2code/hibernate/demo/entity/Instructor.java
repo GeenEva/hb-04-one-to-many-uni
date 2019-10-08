df462607df91a558ed8caf8e10e1c83ac7577a09
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-08 11:46:54 +0200

    Added Review class

--- src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -36,7 +36,7 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
-	@OneToMany(mappedBy="instructor", //referring to property in Course class
+	@OneToMany(mappedBy="instructor", 									//referring to property in Course class
 				cascade= {CascadeType.DETACH, CascadeType.MERGE, 
 						CascadeType.PERSIST, CascadeType.REFRESH })
 	private List<Course> courses;
